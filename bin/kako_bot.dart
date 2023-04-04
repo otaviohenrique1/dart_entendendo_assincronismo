bd42691d11dc1e1bd5e8b7e7c311a5f5de36da00
Otavio Henrique de Lima e Silva <otaviohlsilva@gmail.com>
2023-04-04 19:08:26 -0300

    commit 5

--- bin/kako_bot.dart
+++ bin/kako_bot.dart
@@ -1,17 +1,21 @@
 import 'dart:io';
 import 'questions/time_questions.dart';
+import 'timing/waiting_time.dart';
 
-void main(List<String> arguments) {
+void main(List<String> arguments) async {
   String kakoBot = 'KakoBOT:\n';
   var a = true;
   String usuario = '';
 
   print('-- Iniciando o KakoBOT, aguarde..--');
 
+  await BotClock().clock(2);
+
   print('KakoBOT:\n Oi :) \n Como posso ajudar?');
   do {
     usuario = stdin.readLineSync().toString();
     print('-- Processando pergunta, aguarde..--');
+    await BotClock().clock(2);
     if (usuario.contains('xau') ||
         usuario.contains('Xau') ||
         usuario.contains('Adeus') ||
@@ -21,9 +25,12 @@ void main(List<String> arguments) {
     } else if (TimeQuestions(usuario).isThisTime()) {
       // verificar antes, assim não fazemos toda a função sem precisar.
       TimeQuestions(usuario).timeQuestion();
-    } else if (false) {
+      await BotClock().clock(2);
+    } /* else if (false) {
       //Basta adicionar novas perguntas aqui!
-    } else {
+    } */
+    else {
+      await BotClock().clock(2);
       print(kakoBot +
           ' Não fui treinado para responder a essa pergunta \n Desculpe :( ');
       print(kakoBot + ' Você pode fazer outra pergunta ou dizer Adeus');
